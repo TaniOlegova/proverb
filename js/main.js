ebf6afb43f865caca5bec5840e27129241d2c04a
Tkalenko <tani.tkalenko@yandex.ua>
2017-09-27 20:44:59 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -14,4 +14,31 @@ $(document).ready(function(){
         $('.navigation-menu').stop().slideToggle();
     });
 
+    $('.people-slider').carousel({
+
+        // the number of images to display
+        num: 5,
+
+        // max width of the active image
+        maxWidth: 180,
+
+        // min width of the active image
+        maxHeight: 200,
+
+        // enable auto play
+        autoPlay: false,
+
+        // autoplay interval
+        showTime: 1000,
+
+        // animation speed
+        animationTime: 300,
+
+        // 0.0 - 1.0
+        scale: 0.8,
+
+        // the distance between images
+        distance: 85
+    });
+
 });
